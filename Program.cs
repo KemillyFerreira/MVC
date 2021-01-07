4137c3e1581b304d45bec9fad5b18eae1c4f2afa
Kemilly Ferreira <73208400+KemillyFerreira@users.noreply.github.com>
2021-01-07 11:19:38 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MVC_Console.Controllers;
+using MVC_Console.Models;
+
+namespace MVC_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            //Instanciamos nosso Controller
+            ProdutoController produtoController = new ProdutoController ();
+
+            produtoController.Cadastrar();
+
+            produtoController.MostrarProdutos();
+            
+        }
+    }
+}
